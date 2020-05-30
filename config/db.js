88151ce4db55305891d262e2b491cb8bdcc65aa6
Zeniith <sebastian.binkowski.97@gmail.com>
2020-05-30 19:34:40 +0200

    basic routes

---
+++ config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const db = process.env.DB_ATLAS;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected...");
+  } catch (err) {
+    console.error(err.message);
+    //Exit process with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
